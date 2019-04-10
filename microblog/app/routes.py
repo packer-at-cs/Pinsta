cd539aac1072253108deea839129cb0184213284
Zamien Allard <zaallard@packer.edu>
2019-04-10 11:58:18 -0400

    added two new routes for the profile

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,3 +4,42 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/profile')
+def profile():
+
+	avatar="/static/avatar.jpg"
+	user_information = {
+
+		"user_name": "Jon Doe",
+		"profile_picture": "https://pbs.twimg.com/profile_images/502988973052932096/nvkFAZdJ_400x400.jpeg",
+		"bio": "This is my bio",
+		"posts": {
+		  1: {
+		    "body": "text",
+		    "image": "https://static01.nyt.com/images/2012/05/07/nyregion/PACKER2/PACKER2-jumbo.jpg"
+		  },
+		  2: { 
+		    "body": "text2",
+		    "image": "http://www.nycago.org/Organs/Bkln/img/PackerInstInt1902.jpg"
+		  }
+		}
+	}
+	return render_template("profile.html", user_information=user_information, avatar=avatar)
+
+@app.route("/profile_image", methods=["POST","GET"])
+def profile_images():
+    avatar="/static/avatar.jpg"
+    return render_template("profile_image.html",)
+
+    if request.method=="GET":
+        return render_template("profile.html")
+    else:
+        profilepic = request.form.get("fileupload")
+        avatar=profilepic 
+        return render_template("profile.html", profilepic=profilepic, avatar=profilepic)
+
+
+ 
+ 
