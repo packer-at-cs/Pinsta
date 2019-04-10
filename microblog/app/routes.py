9506fc225c3a6b4b71dae452ca0c2716660315a2
Harrison Resnick <haresnick@packer.edu>
2019-04-10 11:57:30 -0400

    fixes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,25 @@
 from app import app
+import smtplib
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route("/email", methods = ("GET", "POST"))
+def home():
+  if request.method == "GET":
+    return render_template("home.html")
+  else:
+
+    sender_email = "packer.insta@gmail.com"
+    sender_password = "atcompsci"
+    reciever_email = request.form["email"]
+    message = "Boolean Logic"
+
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    s.starttls() 
+    s.login(sender_email, sender_password) 
+    s.sendmail(sender_email, reciever_email, message)
+    s.quit()
+    return render_template("home.html", message = "Message sent!")
