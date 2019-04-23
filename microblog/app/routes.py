26243f0eb496dd7c4568fbe0231e7d1c98efae3a
peter cembalest <30907485+peter+cembalest+@users.noreply.github.com>
2019-04-23 12:58:42 -0400

    added registration

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -43,6 +43,21 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
