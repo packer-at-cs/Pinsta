05801a03d99991506052bf1cee22926ca8222e93
gbenedisgrab <gbenedisgrab@packer.edu>
2019-04-24 14:15:25 -0400

    Added new table for comments based on work I did with Olivia

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -63,6 +63,16 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
