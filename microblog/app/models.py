10bfb1eb7651c2fc2e43113f2ce61145608a00dd
gbenedisgrab <gbenedisgrab@packer.edu>
2019-04-24 11:03:54 -0400

    added new fields to the User model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -17,6 +17,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
