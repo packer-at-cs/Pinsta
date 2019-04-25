81a455c55b66ed9ac8fce75d1418c3d605c552a1
Zamien Allard <zaallard@packer.edu>
2019-04-25 10:56:01 -0400

    added new edit profile function

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -140,8 +140,20 @@ def profile():
 
 
 @app.route("/edit_profile", methods=['POST','GET'])
+@login_required
 def edit_profile():
-    return render_template("edit_profile.html")
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
     
 
 @app.route("/bio_summary", methods=['POST','GET'])
