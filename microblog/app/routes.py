14d26a79fdd08cb7bc98e8b38eadc0b0c9f4f930
Zamien Allard <zaallard@packer.edu>
2019-04-25 11:14:50 -0400

    changed routes.py

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -150,11 +150,11 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('profile'))
+        return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('profile.html', title='Edit Profile',
+    return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
     
 
