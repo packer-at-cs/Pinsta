4624934baa61d968e11fdadd3698475cb045cddc
Zamien Allard <zaallard@packer.edu>
2019-04-25 10:53:51 -0400

    added Edit profile class in form.py

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -33,3 +33,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
